a01a08190000adecbe8d27bb43982d48ad820c0f
HowAboutRyze <lizhongwei157@163.com>
2019-06-13 23:55:00 +0800

    玩转webpack第15课解析ES6和React JSX；

--- code/my-project/src/search.js
+++ code/my-project/src/search.js
@@ -1,1 +1,26 @@
-document.write('Search page');
+'use strict';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Search extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            text: '我是react demo'
+        }
+    }
+
+    render() {
+        return <div onClick={e => (this.state.text = '我很帅')}>{this.state.text}</div>
+    }
+
+    // render() {
+    //     return <div>我是demo</div>
+    // }
+}
+
+ReactDOM.render(
+    <Search />,
+    document.getElementById('root')
+)
