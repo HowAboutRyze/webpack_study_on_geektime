ab22e925300d3da2c56720d87157d8e11b836bd6
HowAboutRyze <lizhongwei157@163.com>
2019-06-17 23:21:32 +0800

    玩转webpack第16课解析css、less、sass

--- code/my-project/src/search.js
+++ code/my-project/src/search.js
@@ -2,17 +2,26 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './search.less';
 
 class Search extends React.Component {
     constructor() {
         super();
         this.state = {
-            text: '我是react demo'
+            text: '我是lzw'
         }
+
+        this.sayCool = this.sayCool.bind(this);
+    }
+
+    sayCool() {
+        this.setState({
+            text: '我很帅'
+        })
     }
 
     render() {
-        return <div onClick={e => (this.state.text = '我很帅')}>{this.state.text}</div>
+        return <div className="lzw" onClick={this.sayCool}>{this.state.text}</div>
     }
 
     // render() {
